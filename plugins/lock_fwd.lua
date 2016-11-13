7f85f31b691dbb44c0190bec4900bd97cd11a740
kiarash <info@Kiava.ir>
2016-11-13 12:24:47 +0330

    Update lock_fwd.lua

--- plugins/lock_fwd.lua
+++ plugins/lock_fwd.lua
@@ -2,10 +2,11 @@ do
 
 local function pre_process(msg)
     
+    --Checking mute
     local hash = 'mate:'..msg.to.id
-    if redis:get(hash) and msg.fwd_from and not is_momod(msg)  then
-	  chat_del_user('chat#id'..msg.to.id,'user#id'..msg.from.id, ok_cb, false)
-            return "fwd was locked"
+    if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg)  then
+            delete_msg(msg.id, ok_cb, true)
+            return "done"
         end
     
         return msg
@@ -15,34 +16,26 @@ local function pre_process(msg)
 
 
 local function run(msg, matches)
-    chat_id = msg.to.id
+    chat_id =  msg.to.id
     
-    if matches[1] == 'lock' and is_momod(msg) then
+    if is_momod(msg) and matches[1] == 'قفل' then
       
             
                     local hash = 'mate:'..msg.to.id
                     redis:set(hash, true)
-                    return "قفل فروارد فعال شد"
-  elseif matches[1] == 'unlock' and is_momod(msg) then
+                    return ""
+  elseif is_momod(msg) and matches[1] == '-قفل' then
                     local hash = 'mate:'..msg.to.id
                     redis:del(hash)
-                    return "قفل فروارد غیر فعال شد"
-					end
-					if matches[1] == 'status' then
-                    local hash = 'mate:'..msg.to.id
-                    if redis:get(hash) then
-                    return "fwd is locked"
-					else 
-					return "fwd is not locked"
+                    return ""
+end
 
 end
-end
-end
+
 return {
     patterns = {
-        '^[/!#](lock) fwd$',
-        '^[/!#](unlock) fwd$',
-		'^[/!#]fwd (status)$',
+        '^(قفل) فروارد$',
+        '^[/!#](-قفل) فروارد$'
     },
     run = run,
     pre_process = pre_process
